23fc486cbab6b745e01c8778b69a54e17d928bce
Shukry Zablah <shukryzablah@gmail.com>
2019-10-17 15:35:02 -0400

    included initial documentation of package

---
+++ R/ValleyBikes.R
@@ -0,0 +1,9 @@
+#' ValleyBikes: A package for exploring data from Pioneer Valley bikeshare.
+#'
+#' The foo package provides an easy way to start exploring data from
+#' the Valley Bike initiative through the `stations` and `routes`
+#' datasets.
+#'
+#' @docType package
+#' @name ValleyBikes
+NULL
---
+++ R/stations.R
@@ -0,0 +1,16 @@
+#' Stations
+#'
+#' A dataset containing the Valley Bike stations.
+#' 
+#'
+#' @format A data frame with 54 rows and 6 variables:
+#' \describe{
+#'   \item{Serial_Num}{identifier for station}
+#'   \item{Address}{station address}
+#'   \item{Station_Name}{station name}
+#'   \item{Num_Docks}{the number of docks in the station}
+#'   \item{Latitude}{latitude coordinate of station}
+#'   \item{Longitude}{longitude coordinate of station}
+#'   
+#' }
+"stations"
